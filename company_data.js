ba0d9985f5b0b02aab05d921aee059df3f991d10
Jankiparmar311 <91945706+Jankiparmar311@users.noreply.github.com>
2021-11-28 20:44:43 +0530

    Create company_data.js

---
+++ company_data.js
@@ -0,0 +1,18 @@
+const company = [
+    {
+        "company id": "c1",
+        "name": "Myntra",
+        "product id": ["p1","p2","p3"]
+    },
+    {
+        "company id": "c2",
+        "name": "Amazon",
+        "product id": ["p2","p1","p3"]
+    },
+    {
+        "company id": "c3",
+        "name": "Flipkart",
+        "product id": ["p3","p2","p1"]
+    }
+];
+module.exports = company;
