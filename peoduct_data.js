5e2094ee29827149decd1f925f7f365d9e9624bf
Jankiparmar311 <91945706+Jankiparmar311@users.noreply.github.com>
2021-11-28 20:43:59 +0530

    Create peoduct_data.js

---
+++ peoduct_data.js
@@ -0,0 +1,27 @@
+const product = [
+    {
+        "product id": "p1",
+        "title": "Watch",
+        "price": "500 Rs.",
+        "category": ["Bussiness","Home"],
+        "company id": "c1",
+        "seller id": ["s1","s1","s1"]
+    },
+    {
+        "product id": "p2",
+        "title": "Dress",
+        "price": "900 Rs.",
+        "category": ["Woman","Baby Girls"],
+        "company id": "c2",
+        "seller id": ["s4","s2","s1"]
+    },
+    {
+        "product id": "p3",
+        "title": "Jeans",
+        "price": "985 Rs.",
+        "category": ["Girls","Boys"],
+        "company id": "c3",
+        "seller id": ["s1","s4","s5"]
+    },
+];
+module.exports = product;
