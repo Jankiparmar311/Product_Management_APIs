a8635406352a4fcb441a4de3b3590fb3202c4eb3
Jankiparmar311 <91945706+Jankiparmar311@users.noreply.github.com>
2021-11-28 20:41:55 +0530

    Create seller_data.js

---
+++ seller_data.js
@@ -0,0 +1,28 @@
+const seller = [
+    {
+        "seller id": "s1",
+        "name": "Janki",
+        "product ids": ["p1","p2","p3"]
+    },
+    {
+        "seller id": "s2",
+        "name": "Tanvi",
+        "product ids": ["p1","p2","p3"]
+    },
+    {
+        "seller id": "s3",
+        "name": "Mahi",
+        "product ids": ["p3","p2","p1"]
+    },
+    {
+        "seller id": "s4",
+        "name": "Kavya",
+        "product ids": ["p1","p2","p3"]
+    },
+    {
+        "seller id": "s5",
+        "name": "Kittu",
+        "product ids": ["p3","p1","p2"]
+    }
+];
+module.exports = seller;
